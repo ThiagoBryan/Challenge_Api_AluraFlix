928f3a1971b38e44d2aa8af48164406cc3493e15
Thiago <tthurler10@gmail.com>
2022-10-10 19:02:15 -0300

    VideosRepository

---
+++ AluraFlix/src/main/java/videos/VideosRepository.java
@@ -0,0 +1,9 @@
+package videos;
+
+
+import br.com.aluraFlix.domain.Videos;
+import org.springframework.data.repository.Repository;
+
+public interface VideosRepository extends Repository<Videos, Long> {
+
+}
