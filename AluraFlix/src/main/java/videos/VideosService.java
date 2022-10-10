03b2198fcb89ca8ef3cf2ad2f03d22783c49e0ed
Thiago <tthurler10@gmail.com>
2022-10-10 20:40:48 -0300

    Método salvar na classe VideosService

--- AluraFlix/src/main/java/videos/VideosService.java
+++ AluraFlix/src/main/java/videos/VideosService.java
@@ -11,5 +11,18 @@ public class VideosService {
 
     private final VideosRepository videosRepository;
 
+    private final VideosFormMapper videosFormMapper;
+
+    public String salvar(VideosForm form) {
+        videosRepository.findByUrl(form.getUrl()).ifPresent(videos -> {
+            throw new VideosException("Video já existe");
+        });
+
+        Videos salvarVideos = videosFormMapper.map(form);
+        videosRepository.save(salvarVideos);
+        return "Video criado com Id: " + salvarVideos.getId();
+
+
+    }
 
 }
