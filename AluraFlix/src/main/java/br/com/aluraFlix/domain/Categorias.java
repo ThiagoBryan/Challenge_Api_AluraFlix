26e948551d1ca95cf050ba07f1f564421b08b262
Thiago <tthurler10@gmail.com>
2022-10-17 16:13:09 -0300

    Entidade Categorias

--- AluraFlix/src/main/java/br/com/aluraFlix/domain/Categorias.java
+++ AluraFlix/src/main/java/br/com/aluraFlix/domain/Categorias.java
@@ -1,2 +1,19 @@
-package br.com.aluraFlix.domain;public class Categorias {
+package br.com.aluraFlix.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Categorias {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String titulo;
+    @NotBlank
+    private String cor;
 }
