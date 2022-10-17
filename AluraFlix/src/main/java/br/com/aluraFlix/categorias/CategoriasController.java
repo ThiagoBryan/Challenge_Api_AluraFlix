278ae2b078885e7128e7a29c22deb3f541794af1
Thiago <tthurler10@gmail.com>
2022-10-17 17:15:55 -0300

    GET todas as Categorias

--- AluraFlix/src/main/java/br/com/aluraFlix/categorias/CategoriasController.java
+++ AluraFlix/src/main/java/br/com/aluraFlix/categorias/CategoriasController.java
@@ -1,14 +1,13 @@
 package br.com.aluraFlix.categorias;
 
+import br.com.aluraFlix.videos.VideosView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/categoria")
@@ -21,4 +20,10 @@ public class CategoriasController {
     public ResponseEntity<String> salvar(@RequestBody @Valid CategoriasForm categoriasForm){
         return new ResponseEntity<String>(categoriasService.salvarCategoria(categoriasForm), HttpStatus.CREATED);
     }
+
+    @GetMapping
+    public ResponseEntity <List<CategoriasView>> listasTodas(){
+        return ResponseEntity.ok(categoriasService.todasCategorias());
+    }
+
 }
